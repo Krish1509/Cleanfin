24f49f20e91043e9885556a6a7d9e1e12c8bdf1a
Shilpa-Hoch <137758395+Shilpa-Hoch@users.noreply.github.com>
2025-01-03 17:10:57 +0530

    content bytes module

---
+++ src/pages/Pages/ContentBytes/add-edit/add.tsx
@@ -0,0 +1,323 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from "react";
+
+//import Components
+import { Card, CardBody, Col, Row, Spinner } from "react-bootstrap";
+import BreadcrumbItem from "../../../../Common/BreadcrumbItem";
+import { Field, Form, Formik } from "formik";
+import RichTextEditor from "../../../../Common/Editor/RichTextEditor";
+import Select from "react-select";
+import * as Yup from "yup";
+import {
+  handleFormData,
+  postRequest,
+} from "../../../../service/fetch-services";
+import ToastAlert from "../../../../helper/toast-alert";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const validationSchema = Yup.object().shape({
+  type: Yup.string().required("Type is required!"),
+  title: Yup.string().required("Title is required!"),
+  description: Yup.string().required("Description is required!"),
+  url: Yup.string().when("type", {
+    is: "url",
+    then: (schema) =>
+      schema.required("URL is required!").url("Invalid URL format!"),
+    otherwise: (schema) => schema.notRequired(),
+  }),
+});
+
+interface FormValues {
+  type: string;
+  title: string;
+  description: string;
+  filePath?: string;
+  url?: string;
+}
+
+const AddContentBytes = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const editId = location?.state !== undefined ? location?.state?.id : "";
+  const user = JSON.parse(localStorage.getItem("user") || "");
+
+  const TypeOptions = [
+    { value: "url", label: "URL" },
+    { value: "video", label: "Video" },
+    { value: "audio", label: "Audio" },
+    { value: "file", label: "File" },
+  ];
+
+  const [loading, setLoading] = useState(false);
+  const [editData, setEditData] = useState<any>({});
+  const [getLoading, setGetLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<any>();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (values: any) => {
+    try {
+      // Submit user details to the backend
+      const formData = new FormData();
+      formData.append("userId", user?._id);
+      formData.append("type", values?.type);
+      formData.append("title", values?.title);
+      formData.append("description", values?.description);
+      formData.append("url", values?.url);
+      if (selectedFile && Object.keys(selectedFile)?.length) {
+        formData.append("filePath", selectedFile);
+      }
+
+      setLoading(true);
+
+      if (editData?._id) {
+        formData.append("id", editData?._id);
+      }
+      const url = editData?._id ? "contentBites/edit" : "contentBites/add";
+      const result = await handleFormData(url, formData);
+
+      if (result) {
+        ToastAlert.success(
+          `Content Bytes ${editData?._id ? "updated" : "created"} successfully`
+        );
+        navigate("/contentBytes");
+      } else {
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error("Error:", error);
+    }
+  };
+
+  const fetchDetail = async () => {
+    try {
+      // Submit user details to the backend
+      setGetLoading(true);
+      const body = {
+        id: editId,
+      };
+      const result = await postRequest("contentBites/view", body);
+
+      if (result) {
+        setEditData(result?.data);
+      }
+      setGetLoading(false);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (editId) {
+      fetchDetail();
+    }
+  }, [editId]);
+
+  const handleFileChange = (e: any, type: string) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const fileType = file.type;
+
+    const allowedTypes: Record<string, string[]> = {
+      file: [
+        "application/pdf",
+        "application/msword",
+        "application/vnd.ms-excel",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      ],
+      audio: ["audio/mpeg", "audio/wav", "audio/ogg"],
+      video: ["video/mp4", "video/ogg", "video/webm"],
+    };
+
+    if (!allowedTypes[type]?.includes(fileType)) {
+      setError(`Invalid file type! Please upload a valid ${type} file.`);
+      setSelectedFile(null);
+      e.target.value = ""; // Clear the file input
+    } else {
+      setError("");
+      setSelectedFile(file);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <BreadcrumbItem
+        mainTitle="Content Bytes"
+        subTitle={`Content Bytes ${editData?._id ? "Edit" : "Add"}`}
+      />
+      <Row>
+        <Col sm={12}>
+          <Card>
+            <CardBody>
+              {getLoading ? (
+                <center className="m-4">Loading...</center>
+              ) : (
+                <div className="container">
+                  <Formik<FormValues>
+                    initialValues={{
+                      type: editData?.type || "url",
+                      title: editData?.title || "",
+                      description: editData?.description || "",
+                      url: editData?.url || "",
+                      filePath: editData?.filePath || "",
+                    }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                  >
+                    {({ errors, touched, values, setFieldValue }) => (
+                      <Form>
+                        <Row>
+                          <Col sm={6}>
+                            <div className="mb-3">
+                              <label className="form-label">Action</label>
+                              <Select
+                                isSearchable={true}
+                                options={TypeOptions}
+                                placeholder="Select"
+                                className="react-select"
+                                classNamePrefix="react-select"
+                                onChange={(value) => {
+                                  setFieldValue("type", value?.value);
+                                  setSelectedFile(null);
+                                  setError("");
+                                }}
+                                value={TypeOptions?.filter(
+                                  (obj) => values?.type === obj.value
+                                )}
+                              />
+                              {errors.type && touched.type ? (
+                                <div className="invalid-feedback d-flex align-items-start">
+                                  {errors.type}
+                                </div>
+                              ) : null}
+                            </div>
+                          </Col>
+                          <Col sm={6}>
+                            <div className="mb-3">
+                              <label className="form-label">File Or URL</label>
+                              {values?.type === "url" ? (
+                                <Field
+                                  type="text"
+                                  className="form-control"
+                                  id="url"
+                                  placeholder="Url"
+                                  name="url"
+                                />
+                              ) : (
+                                <input
+                                  type="file"
+                                  className="form-control"
+                                  id="url"
+                                  name={values?.type}
+                                  onChange={(e) =>
+                                    handleFileChange(e, values?.type)
+                                  }
+                                  accept={
+                                    values?.type === "file"
+                                      ? ".pdf,.doc,.docx,.xls,.xlsx,.ppt,.pptx,.txt,.csv"
+                                      : values?.type === "audio"
+                                      ? "audio/*"
+                                      : values?.type === "video"
+                                      ? "video/*"
+                                      : ""
+                                  }
+                                />
+                              )}
+                              {errors.url && touched.url ? (
+                                <div className="invalid-feedback d-flex align-items-start">
+                                  {errors.url}
+                                </div>
+                              ) : null}
+                              {!error &&
+                              values?.type !== "url" &&
+                              !selectedFile ? (
+                                <div className="invalid-feedback d-flex align-items-start">
+                                  File is required
+                                </div>
+                              ) : null}
+                              {error ? (
+                                <div className="invalid-feedback d-flex align-items-start">
+                                  {error}
+                                </div>
+                              ) : null}
+                            </div>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col sm={12}>
+                            <div className="mb-3">
+                              <label className="form-label">Title</label>
+                              <Field
+                                type="text"
+                                className="form-control"
+                                id="title"
+                                placeholder="Title"
+                                name="title"
+                              />
+                              {errors.title && touched.title ? (
+                                <div className="invalid-feedback d-flex align-items-start">
+                                  {errors.title}
+                                </div>
+                              ) : null}
+                            </div>
+                          </Col>
+                        </Row>
+
+                        <Row>
+                          <Col sm={12}>
+                            <div className="mb-3">
+                              <label className="form-label">Description</label>
+                              <RichTextEditor
+                                value={values?.description}
+                                onChange={(value) =>
+                                  setFieldValue("description", value)
+                                }
+                              />
+                              {errors.description && touched.description ? (
+                                <div className="invalid-feedback d-flex align-items-start">
+                                  {errors.description}
+                                </div>
+                              ) : null}
+                            </div>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <div className="text-end">
+                            <button
+                              type="button"
+                              className="btn btn-outline-secondary me-1"
+                              onClick={() => navigate("/recommendation")}
+                            >
+                              Cancel
+                            </button>
+                            <button
+                              type="submit"
+                              className="btn btn-primary"
+                              disabled={loading}
+                            >
+                              Submit{" "}
+                              {loading ? (
+                                <Spinner className="ml-2" size="sm" />
+                              ) : (
+                                ""
+                              )}
+                            </button>
+                          </div>
+                        </Row>
+                      </Form>
+                    )}
+                  </Formik>
+                </div>
+              )}
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
+};
+
+export default AddContentBytes;
---
+++ src/pages/Pages/ContentBytes/index.tsx
@@ -0,0 +1,167 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+//import Components
+import BreadcrumbItem from "../../../Common/BreadcrumbItem";
+import { Button, Card, CardBody, CardHeader, Form } from "react-bootstrap";
+import { postRequest } from "../../../service/fetch-services";
+import Pagination from "../../../Common/Pagination";
+import { useNavigate } from "react-router-dom";
+
+type ContentBytesData = {
+  _id: string;
+  title: string;
+  description: string;
+  type: string;
+  url?: string;
+  filePath?: string;
+  fileExtension: string;
+};
+
+const ContentBytes = () => {
+  const navigate = useNavigate();
+
+  const [contentBytesData, setContentBytesData] = useState<ContentBytesData[]>(
+    []
+  );
+  const [loading, setLoading] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [entriesPerPage, setEntriesPerPage] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+    setCurrentPage(1); // Reset page to 1 when search query changes
+  };
+
+  const handleEntriesPerPageChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setEntriesPerPage(parseInt(e.target.value));
+    setCurrentPage(1); // Reset page to 1 when entries per page changes
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const fetchContentBytesData = React.useCallback(async () => {
+    try {
+      setLoading(true);
+      const body = {
+        limit: entriesPerPage,
+        page: currentPage,
+        search: searchQuery,
+      };
+      const result = await postRequest("contentBites/list", body, true);
+      const { contentBites, totalPages } = result.data;
+      setContentBytesData(contentBites);
+      setTotalPages(totalPages);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  }, [entriesPerPage, currentPage, searchQuery]);
+
+  useEffect(() => {
+    fetchContentBytesData();
+  }, [entriesPerPage, currentPage, searchQuery]);
+
+  const handleEditDate = (id: string) => {
+    console.log(id);
+    navigate("/contentBytes/edit", { state: { id: id } });
+  };
+
+  return (
+    <React.Fragment>
+      <BreadcrumbItem mainTitle="Content Bytes" subTitle="Content Bytes list" />
+      <div className="col-12 mt-4 pb-4">
+        <Card className="table-card">
+          <CardHeader>
+            <div className="d-sm-flex align-items-center justify-content-between">
+              <h5 className="mb-3 mb-sm-0">Content Bytes list</h5>
+              <Button
+                onClick={() => navigate("/contentBytes/add")}
+                className="btn btn-primary"
+              >
+                Add
+              </Button>
+            </div>
+          </CardHeader>
+          <div className="d-sm-flex align-items-center mt-4">
+            <ul className="list-inline ms-auto my-1 me-4">
+              <li className="list-inline-item">
+                <form className="form-search">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search...."
+                    className="ps-2 pe-3 pt-2"
+                    onChange={handleSearchChange}
+                  />
+                </form>
+              </li>
+            </ul>
+          </div>
+          {loading ? (
+            <center className="m-4">Loading...</center>
+          ) : (
+            <React.Fragment>
+              <CardBody className="pt-3">
+                <div className="table-responsive">
+                  <table className="table table-hover" id="pc-dt-simple">
+                    <thead>
+                      <tr>
+                        <th>Sr No</th>
+                        <th>Title</th>
+                        <th>Type</th>
+                        <th>Description</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {contentBytesData?.map((item, key) => (
+                        <tr key={key}>
+                          <td>{key + 1}</td>
+                          <td>{item?.title}</td>
+                          <td>{item?.type}</td>
+                          <td className="html-content">
+                            <span
+                              dangerouslySetInnerHTML={{
+                                __html: item?.description,
+                              }}
+                            />
+                          </td>
+                          <td>
+                            <div className="d-flex align-items-center">
+                              <Button
+                                type="button"
+                                className="avtar avtar-xs btn-link-secondary"
+                                onClick={() => handleEditDate(item?._id)}
+                              >
+                                <i className="ti ti-edit f-20"></i>
+                              </Button>
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </CardBody>
+            </React.Fragment>
+          )}
+          <Pagination
+            totalPages={totalPages}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+            entriesPerPage={entriesPerPage}
+            onEntriesPerPageChange={handleEntriesPerPageChange}
+          />
+        </Card>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ContentBytes;
